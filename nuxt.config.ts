7721e9e8bcc55bf8d589ae79921a318422aad06d
Joao Lopes <joao.lopes@showpad.com>
2026-03-30 10:31:58 +0100

    enforcing netlify nitro rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+
+  ssr: true,
+  nitro: {
+    preset: 'netlify',
+  },
+
+  css: ['~/assets/css/main.css'],
   app: {
     baseURL: '/tools/',
   },
   devtools: { enabled: true },
 
-  css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/a11y',
     '@nuxt/eslint',
